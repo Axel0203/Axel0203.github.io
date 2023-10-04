a20c9ce5987d8e7b893a3eea8efc3f675223b9aa
Axel <98823712+Axel0203@users.noreply.github.com>
2023-10-04 15:56:36 +0200

    copia mywebsite in vuejs

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="container">
+        <div class="main">
+            <div id="text">
+                <h1>Axel Holberg <br> Ferrufino Arze</h1>  
+            </div>
+            <div id="photo">
+                <a href="https://www.instagram.com/p/CknfZ8RN15o/?utm_source=ig_web_copy_link" target="_blank"><img src="/pic.png" alt="photo" ></a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="container">
+        <div class="projects">
+            <h1>Things i did</h1>
+            <ul>
+                <li><a href="https://axel0203.github.io/cube/">- Cube</a></li>
+                <li><a href="https://axel0203.github.io/cubes/">- Cubes!</a></li>
+                <li><a href="https://axel0203.github.io/space-game/">- Three.js game</a></li>
+                <li><a href="https://axel0203.github.io/myRoom/">- Three.js portfolio</a></li>
+            </ul>
+            
+        </div>
+    </div>
+</template>
